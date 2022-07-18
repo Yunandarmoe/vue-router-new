026e2d6cea4f0d42221dac60afc507b758b64f19
YuNandarMoe <scm.yunandarmoe@gmail.com>
2022-07-18 21:46:40 +0630

    Vue Router

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <menu-bar></menu-bar>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import MenuBar from "@/components/MenuBar";
+export default {
+  name: "App",
+  components: {
+    MenuBar
+  }
+}
+</script>
---
+++ src/views/Login.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container mt-5">
+    <div class="row justify-content-center">
+      <div class="col-md-4">
+        <div class="card">
+          <div class="card-body">
+            <div class="alert alert-danger" v-if="message">{{ message }}</div>
+            <h3 class="mt-2 mb-3">Login</h3>
+            <form @submit.prevent="login">
+              <div class="form-group">
+                <input type="text" v-model="email" class="form-control" placeholder="Email">
+              </div>
+              <div class="form-group">
+                <input type="password" v-model="password" class="form-control" placeholder="Password">
+              </div>
+              <button type="submit" class="btn btn-primary btn-block">Login</button>
+            </form>
+          </div>
+          <div class="card-footer">
+            <small><span class="text-muted">Don't have an account?</span> <a href="#">Register</a></small>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {
+      email: "",
+      password: "",
+      message: ""
+    }
+  },
+  methods: {
+    login() {
+      if(this.email === "admin@admin.com" && this.password === "password") {
+        localStorage.setItem('auth', this.email);
+        this.$router.push('/profile');
+      } else {
+        this.message = "Email or Password is incorrect.";
+      }
+    }
+  }
+}
+</script>
